d2b9c967aac6d86dcdc818473ef76bab865254bf
Ahmed Adel <ahmed.adel@tactful.ai>
2023-12-03 05:19:34 +0200

    add swagger docs

--- src/transcation/dto/transaction.dto.ts
+++ src/transcation/dto/transaction.dto.ts
@@ -23,3 +23,47 @@ export class TransactionDto {
     })
     readonly targetCurrency: string; //TODO add enums for the values
 }
+
+export class TransactionReturn {
+        @ApiProperty({
+            description: 'id of the transaction',
+            example: '656befd981c5249358e0d9ce',
+        })
+        id: string;
+        @ApiProperty({
+            description: 'amount of money that needs to tranfer',
+            example: 20,
+        })
+        amount: number;
+        
+        @ApiProperty({
+            description: 'transfered amount',
+            example: 316.1,
+        })
+        convertedAmount: number;
+        @ApiProperty({
+            description: 'source currency',
+            example: "USD",
+        })
+        sourceCurrency: string;
+        @ApiProperty({
+            description: 'target currency',
+            example: "EGP",
+        })
+        targetCurrency: string;
+        @ApiProperty({
+            description: 'target currency',
+            example: "656bef2681c5249358e0d9c8",
+        })
+        user: string;
+        @ApiProperty({
+            description: 'amount of money that needs to tranfer to',
+            example: "2023-12-03T03:02:49.308Z",
+        })
+        createdAt: Date;
+        @ApiProperty({
+            description: 'amount of money that needs to tranfer to',
+            example: "2023-12-03T03:02:49.308Z",
+        })
+        updatedAt: Date;
+}
--- src/transcation/transcation.controller.ts
+++ src/transcation/transcation.controller.ts
@@ -1,20 +1,28 @@
 import { Body, Controller, UseGuards, Post, Req, Get } from '@nestjs/common';
 import { TransactionSerive } from './transcation.service';
-import { TransactionDto } from './dto/transaction.dto';
+import { TransactionDto, TransactionReturn } from './dto/transaction.dto';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiCreatedResponse } from '@nestjs/swagger';
 @Controller('')
 export class TranscationController {
     constructor(private transactionSerive: TransactionSerive) { }
 
     @ApiBearerAuth()
     @Post('/convert')
+    @ApiCreatedResponse({
+        description: 'Created user object as response',
+        type: TransactionReturn,
+    })
     @UseGuards(AuthGuard())
     async convert(@Body() trasnsactioDto: TransactionDto, @Req() req): Promise<any> {
         return await this.transactionSerive.convert(trasnsactioDto, req.user);
     }
     @ApiBearerAuth()
     @Get('/history')
+    @ApiCreatedResponse({
+        description: 'Created user object as response',
+        type: [TransactionReturn],
+    })
     @UseGuards(AuthGuard())
     async getHistory(@Req() req): Promise<any> {
         return await this.transactionSerive.getTransactionHistory(req.user);
